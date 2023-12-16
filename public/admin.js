406fa401be3c0e9275f5f519491207b6c68cf48b
jaleesarobinson84 <147563229+jaleesarobinson84@users.noreply.github.com>
2023-12-16 11:19:09 -0500

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
+async function main() {
+    const response = await fetch('http://localhost:3001/listBooks')
+    const data = await response.json()
 
-// Your Code Here
+   data.forEach(renderBook)
+}
+
+function renderBook(book) {
+    const root = document.getElementById('root')
+
+    const li = document.createElement('li')
+    li.textContent = book.title
+    ']'
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+
+    const saveBtn = document.createElement('button')
+    saveBtn.textContent = 'Save'
+
+    saveBtn.addEventListener('click', async () => {
+        await fetch(`http://localhost:3001/updateBook`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value
+            })
+        })
+    })
+
+    li.append(input, saveBtn)
+
+    root.append(li)
+}
+
+main()
+
